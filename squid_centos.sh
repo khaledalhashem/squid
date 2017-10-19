7dd4afcf7d870066116808675644fe91091725c7
khaledalhashem <kmalhashem@gmail.com>
2017-10-19 11:17:42 +0300

    updated script slightly

--- squid_centos.sh
+++ squid_centos.sh
@@ -6,18 +6,18 @@
 # Blog: https://blog.hostonnet.com
 # Edits: Khaled AlHashem
 # Site: https://naur.us
+# Version 0.1
 
 squid_user=
 squid_password=
 
-yum update
 yum -y install squid httpd-tools
 
 htpasswd -b -c /etc/squid/passwd $squid_user $squid_password
 
-#/bin/rm -f /etc/squid/squid.conf
+mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
 touch /etc/squid/blacklist.acl
-#/usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf  https://github.com/khaledalhashem/squid/raw/master/squid_centos.conf
+wget -O /etc/squid/squid.conf  https://github.com/khaledalhashem/squid/raw/master/squid_centos.conf
 
 iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
 #/sbin/iptables-save
